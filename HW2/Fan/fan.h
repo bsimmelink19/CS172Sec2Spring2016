e050c2dbfae424d8c6654787eb17758fba56718a
bsimmelink19 <bsimmelink19@my.whitworth.edu>
2016-03-08 13:59:53 -0800

    Re-Upload of Homework 2

---
+++ HW2/Fan/fan.h
@@ -0,0 +1,12 @@
+//a class of objects with a speed, an on or off, and a radius
+class fan
+{
+public:
+	//the member variables of the fans
+	int speed;
+	bool on;
+	double radius;
+
+	//the constructor
+	fan();
+};
---
+++ HW2/HW2/evenNumber.h
@@ -0,0 +1,12 @@
+class evenNumber
+{
+public:
+	int value;
+
+	evenNumber();
+	evenNumber(int newValue);
+	int getValue();
+	int getNext();
+	int getPrevious();
+
+};
